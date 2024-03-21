f40c31168f6292c5b7b61fd150e0791a8cf01a91
Aamir Khan Channa <160345903+aamirkhanchanna@users.noreply.github.com>
2024-03-21 12:35:59 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+// // 1 Function
+// function halfFryEgg(){
+//     let cooked = 1 + 1.5 + 2;
+//     //   Egg + Butter + Salt
+//     console.log(cooked);
+// }
+// halfFryEgg()
+// // Invoking the function
+// // 2 Returning Function Value
+// function halfFryEgg():number{
+//     let cooked:number = 1 + 1.5 + 2;
+//     //   Egg + Butter + Salt
+//     // console.log(cooked);
+//     return cooked
+// }
+// const egg = halfFryEgg()
+// console.log(egg);
+// // 3 Parameters and Argument
+// // 1st Way
+// function halfFryEgg(egg: number, butter: number, salt: number): number {
+//     return egg + butter + salt;
+// }
+// let response: number = halfFryEgg(1, 1.5, 2)
+// console.log(response)
+// // 2nd Way
+// function myTeachers(teacher1:string, teacher2:string, teacher3:string, teacher4:string){
+//     return teacher1 + teacher2 + teacher3 + teacher4
+// }
+// const response = myTeachers("Asharib","Ubaid","Hafsa","Naeem")
+// console.log(response);
